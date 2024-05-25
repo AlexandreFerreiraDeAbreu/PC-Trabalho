3e960bbc710dc4465d37b29d7db9bd530ed3bf7d
Alex <alex_abreu2@hotmail.com>
2024-05-25 22:11:58 +0100

    fica preso no play, planetas a funcionar

--- Planeta.java
+++ Planeta.java
@@ -10,10 +10,12 @@ public class Planeta {
     int width;
 
 
-    public Planeta(int raio, float posX, float posY, float velX, float velY, int height, int width){
+    public Planeta(int raio, float posX, float posY, float velX, float velY, int width, int height){
         this.raio = raio;
         this.pos = new PVector(posX, posY);
         this.vel = new PVector(velX, velY);
+        this.width = width;
+        this.height = height;
     }
 
     public PVector getPos(){
@@ -25,16 +27,20 @@ public class Planeta {
     }
 
     public void update(){
-        float dX = width/2 - pos.x;
-        float dY = height/2 - pos.y;
+        float dX = this.width/2 - pos.x;
+        float dY = this.height/2 - pos.y;
         float distQuadrado = dX*dX + dY*dY;
         double distancia = Math.sqrt(distQuadrado);
         float forca = constG / distQuadrado;
+        if (forca > 2) forca = 2;
+        if (forca < -2) forca = -2;
         double aceleracaoX = forca * dX / distancia;
         double aceleracaoY = forca * dY / distancia;
 
         vel.x += aceleracaoX;
         vel.y += aceleracaoY;
+        
+        //System.out.println(forca);
 
         pos.add(vel);
     }
